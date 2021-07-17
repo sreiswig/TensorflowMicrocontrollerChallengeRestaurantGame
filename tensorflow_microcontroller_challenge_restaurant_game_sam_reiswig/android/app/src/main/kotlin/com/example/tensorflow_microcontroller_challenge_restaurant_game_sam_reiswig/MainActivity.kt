f193a8dc08dc7de334ef12f83fc7a3e21e740137
sam_reiswig <sam.reiswig@gmail.com>
2021-07-17 16:49:04 -0700

    Create New Flutter Project

---
+++ tensorflow_microcontroller_challenge_restaurant_game_sam_reiswig/android/app/src/main/kotlin/com/example/tensorflow_microcontroller_challenge_restaurant_game_sam_reiswig/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tensorflow_microcontroller_challenge_restaurant_game_sam_reiswig
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
